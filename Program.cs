ad9691ea61b829935009c972d74c664e230d0338
ivan <ivanpanferov2016@mail.ru>
2023-03-22 20:12:55 +0300

    added method fillresultarray

--- Program.cs
+++ Program.cs
@@ -20,4 +20,23 @@ int ArrayCheck(string[] array)
     }
  return count;
 }
-
+string[] FillResultArray(string[] array, int count)
+{
+ string[] result = new string[count];
+ for (int i = 0; i < array.Length; i++)
+    {
+ if (array[i].Length <= 3)
+        {
+ result[result.Length - count] = array[i];
+ count--;
+        }
+    }
+ return result;
+}
+void PrintArray(string[] array)
+{
+ Console.Write("[");
+ for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+ Console.Write($"{array[array.Length - 1]}]");
+}
+PrintArray(resultArray);
