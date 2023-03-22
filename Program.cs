338e73f75ece044c50ddea961cf5c7ec1dfdd679
ivan <ivanpanferov2016@mail.ru>
2023-03-22 20:12:19 +0300

    added method arraycheck

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
+"-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
+int count = ArrayCheck(array);
+string[] resultArray = FillResultArray(array, count);
+int ArrayCheck(string[] array)
+{
+ int count = 0;
+ for (int i = 0; i < array.Length; i++)
+    {
+ if (array[i].Length <= 3) count++;
+    }
+ return count;
+}
+
